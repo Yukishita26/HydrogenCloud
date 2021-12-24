17a8c2442c4620dea2d0d97c95c449790a42ef51
Yukishita <33201164+Yukishita26@users.noreply.github.com>
2021-12-24 15:06:19 +0900

    add: complex phase, example image

--- produire/hydro_shower_c.py
+++ produire/hydro_shower_c.py
@@ -16,7 +16,11 @@ for target_name in target_names:
 plots = np.asarray(plots)
 
 from mpl_toolkits.mplot3d import Axes3D
-def plot_3dp(X,Y,Z,P, _range=5):
+import cmath
+import colorsys
+def plot_3dp(X,Y,Z,PR,PI, _range=5):
+    #print([cmath.phase(r+1j*i))/cmath.pi/2+0.5 for r,i in zip(PI,PR)])
+    print([colorsys.hsv_to_rgb(cmath.phase(r+1j*i)/cmath.pi/2+0.5,1,1) for r,i in zip(PI,PR)])
     fig = plt.figure(figsize=(6,6))
     ax = Axes3D(fig)
     ax.set_xlabel("X")
@@ -24,17 +28,14 @@ def plot_3dp(X,Y,Z,P, _range=5):
     ax.set_zlabel("Z")
     ax.scatter(X, Y, Z,
             marker=".", s=0.5,
-            c=[(1,0,0) if p>0 else (0,0,1) for p in P], alpha=0.5)
-    #ax.plot(X[P>0], Y[P>0], Z[P>0],
-    #        marker=".", markersize=0.5, linestyle='None', color="blue", alpha=0.5)
-    #ax.plot(X[P<0], Y[P<0], Z[P<0],
-    #        marker=".", markersize=0.5, linestyle='None', color="red", alpha=0.5)
+            c=[colorsys.hsv_to_rgb(cmath.phase(r+1j*i)/cmath.pi/2+0.5,1,1)
+              for r,i in zip(PI,PR)], alpha=0.5)
     ax.set_xlim(-_range, _range)
     ax.set_ylim(-_range, _range)
     ax.set_zlim(-_range*0.75, _range*0.75)
 
 for i,_ in enumerate(target_names):
-	plot_3dp(plots[i, ::skip_step, 0], plots[i, ::skip_step, 1], plots[i, ::skip_step, 2], plots[i, ::skip_step, 3], _range=8)
+	plot_3dp(plots[i, ::skip_step, 0], plots[i, ::skip_step, 1], plots[i, ::skip_step, 2], plots[i, ::skip_step, 3], plots[i, ::skip_step, 4], _range=8)
 plt.show()
 
 #plt.figure(figsize=(6,6))
